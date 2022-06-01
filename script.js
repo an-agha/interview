f2cba842e33c1c7d8b098176f628c4e5aa6cffc6
an-agha <anaghanisakar@gmail.com>
2022-06-01 15:42:17 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function showItem(){
+    var para = document.getElementById("nav-show");
+    para.classList.toggle("nav-class");
+}
+
+function hide(){
+    // document.getElementById("nav-show").style.display='none';
+    var para = document.getElementById("nav-show");
+    para.classList.toggle("nav-class");
+}
+const demo = document.getElementById('demoClass');
+demo.addEventListener('click', function(){
+    location.href="https://www.youtube.com/results?search_query=team+naach";
+})
